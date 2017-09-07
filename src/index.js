42c0efaa37b3cee25dde42d226ee795718bdd5fd
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2017-09-07 12:56:12 +0200

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,77 @@
+'use strict';
+import Subject from './subject';
+
+let store;
+let internalState = {};
+let internalReducer = () => {
+};
+let internalSubjects = {};
+
+/**
+ * Broadcast functions
+ */
+function broadcastAll(subjects) {
+	Object.keys(subjects).forEach(subject => {
+		subject.next(internalState[subject]);
+	});
+}
+
+function broadcast(subject) {
+	internalSubjects[subject].next(internalState[subject]);
+}
+
+/**
+ * Configure store
+ */
+function configure(reducer, state, subjects) {
+	internalState = state;
+	internalReducer = reducer;
+	internalSubjects = subjects;
+}
+
+/**
+ * Internal store class
+ */
+class AppStore {
+	constructor(reducer, state, subjects) {
+		configure(reducer, state, subjects);
+		console.log('Store initialized!');
+	}
+
+	subscribe(observer) {
+		Object.keys(internalSubjects).forEach(subject => {
+			internalSubjects[subject].subscribe(observer);
+			observer.next(internalState[subject]);
+		});
+	}
+
+	unsubscribe(observer) {
+		Object.keys(internalSubjects).forEach(subject => {
+			internalSubjects[subject].unscribe(observer);
+		});
+	}
+
+	select(subtype) {
+		return internalSubjects[subtype];
+	}
+
+	dispatch(action, payload) {
+		internalReducer(action, payload);
+	}
+}
+
+/**
+ * Plugin
+ */
+const VeamsStore = {
+	pluginName: 'Store',
+	initialize: function (Veams, options) {
+		store = new AppStore(options.reducer, options.state, options.subjects);
+	}
+};
+
+/**
+ * Exports
+ */
+export {broadcast, broadcastAll, internalState as state, store, Subject};
+export default VeamsStore;
---
+++ src/subject.js
@@ -0,0 +1,21 @@
+export default class Subject {
+	constructor() {
+		this.observers = [];
+	}
+
+	subscribe(obs) {
+		this.observers.push(obs);
+	}
+
+	unsubscribe(obs) {
+		this.observers.forEach((observer, idx) => {
+			if (observer === obs) {
+				this.observers.splice(idx, 1);
+			}
+		});
+	}
+
+	next(data) {
+		this.observers.forEach(obs => obs.next(data));
+	}
+}
